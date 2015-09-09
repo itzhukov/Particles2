ef0ba1b6d0dfddcb8aeb3740faae7e945fd459e6
Vadim Zhukov <zhukov@easykod.ru>
2015-09-09 20:00:46 +0300

    red particles

--- js/main.js
+++ js/main.js
@@ -6,22 +6,30 @@ document.onreadystatechange = function () {
 			c = canvas.getContext('2d'),
 			particles = {},
 			particleId = 0;
-			particleCount = 5;
+			particleCount = 1,
+			emitterSquare = 500;
 
-		canvas.width = 800;
+
+		canvas.width = 1200;
 		canvas.height = 600;
 
 		document.body.appendChild(canvas);
 
+		var img = document.getElementById("particle");
+
 		c.fillStile = "black";
 		c.fillRect(0, 0, canvas.width, canvas.height);
 
+		/* Particle */
 		function Particle() {
-			this.x = canvas.width/2;
-			this.y = canvas.height/2;
+			this.x = (canvas.width/2 - emitterSquare/2) + (Math.random() * emitterSquare);
+			this.y = (canvas.height/2 - emitterSquare/2) + (Math.random() * emitterSquare);
 
-			this.vx = Math.random() * 10 - 5;
-			this.vy = Math.random() * 10 - 5;
+			this.vx = Math.random() * 3 - 3;
+			this.vy = Math.random() * 3 - 3;
+
+			this.width = 5;
+			this.height = 5;
 
 			this.gravity = 0.2;
 
@@ -30,33 +38,64 @@ document.onreadystatechange = function () {
 			this.id = particleId;
 
 			this.life = 0;
-			this.maxLife = Math.random() * 30 + 10 ;
+			this.maxLife = Math.random() * 30 + 50;
+			/*
+			this.color = "rgba("+ parseInt(Math.random()*255, 10) + "," +
+				                + parseInt(Math.random()*255, 10) + "," +
+				                + parseInt(Math.random()*255, 10) + ", 0.5)";
+			*/
+			this.color = "rgba("+ parseInt(Math.random()*255, 10) + ", 0, 0, 0.9)";
 
+
+
+			// console.log('color:' + this.color);
 		}
 
+
+		/* Draw particle */
 		Particle.prototype.draw = function(){
 			this.x += this.vx;
 			this.y += this.vy;
 
-			this.vy += this.gravity;
 
+			if (Math.random() < 0.1){
+				this.vx += Math.random() * 10 - 5;
+				this.vy += Math.random() * 10 - 5;
+			}
+
+
+			//this.vy += this.gravity;
+/*
+			if (Math.random() > 0.3){
+				this.vx += Math.random() * 0.1;
+				this.vy += Math.random() * 0.1;
+			}
+*/
 			this.life++;
 
 			if (this.life >= this.maxLife){
 				delete particles[this.id];
 			}
 
-			c.fillStyle = "white";
-			c.fillRect(this.x, this.y, 5, 5);
+			//c.fillStyle = "rgba(255, 0, 0, 0.1)";
+			//c.fillStyle = "rgba(255, 255, 255, 0.5)";
+			c.fillStyle = this.color;
+			c.fillRect(this.x, this.y, this.width, this.height);
+
+			//c.drawImage(img, this.x, this.y, 68, 85);
+
+
 		};
 
 
+		// new particles
 		for (var i = 0; i < particleCount; i++){
-			new Particle(20);
+			new Particle();
 		}
 
+
 		setInterval(function(){
-			c.fillStyle = "black";
+			c.fillStyle = "rgba(0, 0, 0, 0.1)";
 			c.fillRect(0, 0, canvas.width, canvas.height);
 
 			for (var i = 0; i < particleCount; i++){
@@ -68,6 +107,7 @@ document.onreadystatechange = function () {
 			}
 
 		}, 30);
+
 
 	}
 }
